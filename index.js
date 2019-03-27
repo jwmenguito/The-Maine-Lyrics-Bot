d40542f7922d838b6f10ff3324c0c35a12e0003b
jmenguito <jamespaolo.menguito@gmail.com>
2019-03-27 11:35:53 +0800

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 var controller = require('./src/config/controller.js');
-var port = process.env.port || 3000;
-controller.onLogin;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log("App is running on port "+ port);
