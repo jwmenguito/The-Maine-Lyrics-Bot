0511b5e4382c8eaa6da1ef8d0e158f3a51b5d941
jmenguito <jmenguito@exist.com>
2019-10-07 10:00:51 +0800

    Update time interval

--- src/config/controller.js
+++ src/config/controller.js
@@ -1,6 +1,6 @@
 var service = require('./services/songServices.js');
 var scheduler = require('node-schedule');
-var tweet = scheduler.scheduleJob('0 0 3 * * *', service.tweet());
+var tweet = scheduler.scheduleJob('0 0 */3 * * *', service.tweet());
 var onLogin = service.onLogin();
 exports.onLogin = onLogin;
 exports.tweet = tweet;
